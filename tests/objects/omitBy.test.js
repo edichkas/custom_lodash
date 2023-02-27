f6ac4bfeb28a612551496415528752caea804b59
Edichkas <edichkas@gmail.com>
2023-02-27 15:12:49 +0200

    (feat: Objects): Create omitBy method

---
+++ tests/objects/omitBy.test.js
@@ -0,0 +1,19 @@
+
+/*
+var object = { 'a': 1, 'b': '2', 'c': 3 };
+ 
+_.omitBy(object, _.isNumber);
+// => { 'b': '2' }
+*/
+const omitBy = require('../../src/objects/omitBy');
+
+var object = { 'a': 1, 'b': '2', 'c': 3 };
+
+function predicate(value) {
+    if (Number.isFinite(value)) return true;
+    return false;
+}
+
+test('test for omitBy function', () => {
+    expect(omitBy(object, predicate)).toStrictEqual({ 'b': '2' });
+});
